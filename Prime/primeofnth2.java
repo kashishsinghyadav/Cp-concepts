5fb632229e3aca3b3f4d93f929a505ff701fd6e7
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-06-27 20:54:04 +0530

    Create primeofnth2.java

---
+++ Prime/primeofnth2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Prime {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        
+        int sqrtN = (int) Math.sqrt(n);
+        for (int i = 5; i <= sqrtN; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isPrime(n));
+    }
+}
