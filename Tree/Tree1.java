76ae605566690a5f3c253671d6dec6ed8f9df514
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-07-22 21:51:46 +0530

    Create Tree1.java

---
+++ Tree/Tree1.java
@@ -0,0 +1,65 @@
+public class BinaryTrees {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+        public static void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.println(root.data);
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public static void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.println(root.data);
+            inorder(root.right);
+        }
+
+        public static void postorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.println(root.data);
+        }
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.buildTree(nodes);
+        bt.preorder(root);
+        bt.inorder(root);
+        bt.postorder(root);
+    }
+}
